4e413ff6d3c81827031b0db41fb649431ee1f24d
Gabrielly Vitoria Da Silva Moraes - SENAI - Cuiaba/MT <gabrielly.moraes@alunosenai.mt>
2026-07-24 11:06:40 -0400

    fatorial

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numero = 8
+numero = 5
+fatorial = 1
 
-for i in range(1, 11):
-    print(f"{numero} x {i} = {numero * i}")
+for i in range(1, numero + 1):
+    fatorial *= i
 
+print("Fatorial:", fatorial)
